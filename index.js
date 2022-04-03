b8b303b3af855901f99aaab1818ccf1d4d466435
amenov <amenov.abdulsalam@gmail.com>
2022-04-04 01:06:06 +0600

    update

--- index.js
+++ index.js
@@ -1,12 +1,4 @@
-const fs = require('fs')
-const lodash = require('lodash')
-const path = require('path')
-
-const libPath = __dirname + '/lib'
-const lib = {}
-
-for (const file of fs.readdirSync(libPath)) {
-  lib[lodash.camelCase(path.parse(file).name)] = require(libPath + '/' + file)
+module.exports = {
+  pagination: require('./lib/pagination'),
+  paramsIsNotNaN: require('./lib/params-is-not-nan'),
 }
-
-module.exports = lib
